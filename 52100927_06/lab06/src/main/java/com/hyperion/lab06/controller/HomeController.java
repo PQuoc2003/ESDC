5ca261be379daa56dd5c006c510286aeffc8b5e4
Phú Quốc <dinhphuquoc2003@gmail.com>
2024-10-30 20:20:31 +0700

    fix bug ~~

---
+++ 52100927_06/lab06/src/main/java/com/hyperion/lab06/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.hyperion.lab06.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHomepage(){
+        return "Login success";
+    }
+
+
+    @RequestMapping("/user")
+    public Principal user(Principal user){
+        return  user;
+    }
+}
