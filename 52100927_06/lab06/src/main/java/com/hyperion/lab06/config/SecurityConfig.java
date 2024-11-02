3a9f1b4c288cc8c9b62e3292f938df7bd53760bf
Phú Quốc <dinhphuquoc2003@gmail.com>
2024-11-02 10:01:26 +0700

    lab07 - finish ~~

--- 52100927_06/lab06/src/main/java/com/hyperion/lab06/config/SecurityConfig.java
+++ 52100927_06/lab06/src/main/java/com/hyperion/lab06/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +26,13 @@ public class SecurityConfig {
                         login
                                 .loginPage("/login").permitAll()
                                 .defaultSuccessUrl("/home")
+                )
+                .logout(logout ->
+                        logout.invalidateHttpSession(true)
+                                .clearAuthentication(true)
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .logoutSuccessUrl("/")
+                                .permitAll()
                 )
                 .build();
 
