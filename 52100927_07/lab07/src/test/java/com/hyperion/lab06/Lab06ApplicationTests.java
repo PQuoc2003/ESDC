2c38ec8a59ac545d40780d803105e90913d4d583
Phú Quốc <dinhphuquoc2003@gmail.com>
2024-11-02 10:08:27 +0700

    reserve lab06

---
+++ 52100927_07/lab07/src/test/java/com/hyperion/lab06/Lab06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperion.lab06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
